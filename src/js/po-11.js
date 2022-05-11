9ce23ba532da869699251375999d91797001e6b3
Nathan Strutz <strutz@gmail.com>
2022-05-11 15:51:12 -0400

    Fix nav highlighting

--- src/js/po-11.js
+++ src/js/po-11.js
@@ -1,5 +1,5 @@
 const nav = document.getElementById("primary-nav");
-const navLinks = nav.getElementsByTagName("a");
+var navLinks = nav.getElementsByTagName("a");
 for (link of navLinks) {
   let linkHref = link.href.split(/\//)[3];
   let addrHref = window.location.href.split(/\//)[3];
